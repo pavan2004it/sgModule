515fea46bccc0ee5c655050969149f2436b6d5ff
Pavan Kumar <pavan_2004it@icloud.com>
2023-08-18 17:31:19 +0530

    removed the public sg

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,10 @@
 output "private-sg-ecs" {
   value = aws_security_group.PrivateEcsSg.id
 }
-output "alb-sg-ecs" {
-  value = aws_security_group.AlbEcsSg.id
-}
 
 output "alb-sg" {
   value = aws_security_group.AlbSg.id
 }
-
 
 output "rds-sg" {
   value = aws_security_group.RDS-Sg.id
